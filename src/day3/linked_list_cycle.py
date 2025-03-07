35421f7ce063f7a521e03a336c0be70731344258
Clay Hilgert <claytonhilgertmst@gmail.com>
2025-03-06 20:09:42 -0600

    day2 1

---
+++ src/day3/linked_list_cycle.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/linked-list-cycle/description/
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        
+def hasCycle(head) -> bool:
+    if not head or not head.next:
+        return False
+    slow = head
+    fast = head.next
+    while (fast.next and fast.next.next):
+        if slow == fast:
+            return True
+        slow = slow.next
+        fast = fast.next.next
+    return False
